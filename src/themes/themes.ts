b0a10078493f2fa518a3cc1b14b804bd36f0dffa
Fabricio Menezes de Carvalho <fabriciomdec@gmail.com>
2023-01-25 19:14:14 -0300

    feat: :sparkles: add theme to app

---
+++ src/themes/themes.ts
@@ -0,0 +1,28 @@
+export default {
+  COLORS: {
+    GRAY_100: '#E1E1E6',
+    GRAY_200: '#C4C4CC',
+    GRAY_300: '#7C7C8A',
+    GRAY_400: '#323238',
+    GRAY_500: '#29292E',
+    GRAY_600: '#202024',
+    GRAY_700: '#121214',
+
+    PURPLE: '#8284FA',
+    PURPLE_DARK: '#5E60CE',
+
+    BLUE: '#4EA8DE',
+    BLUE_DARK: '#1E6F9F',
+
+    Danger: '#E25858'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Inter_400Regular',
+    BOLD: 'Inter_700Bold'
+  },
+  FONT_SIZE: {
+    SM: 12,
+    MD: 14,
+    LG: 16
+  }
+}
